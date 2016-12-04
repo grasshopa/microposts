c81e36436bc0834f1860e48e90696c14d0321576
grasshopa <coucicouca.web@gmail.com>
2016-12-04 10:25:00 +0000

    kadai_update3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  #ログインしているユーザーが自分の編集画面のみに編集・更新を限定する
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   
   #ユーザーが情報を入力してログインした後の画面
   def show
@@ -12,7 +15,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user  # redirect_to user_path(@user)と同じ
     else
@@ -21,15 +23,17 @@ class UsersController < ApplicationController
   end
   
   def edit
+    #userへの代入文を削除  
     @user = User.find(params[:id])
   end
   
   def update
+    #userへの代入文を削除  
     @user = User.find(params[:id])
     #user_attributesを使って送信されたparamsに基づいてユーザを更新
     if @user.update_attributes(user_params)
       redirect_to @user
-      flash[:success] = "updated" 
+      flash[:success] = "Updated" 
     else
       render 'edit'
     end
@@ -38,7 +42,22 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name,:email,:password,
+    params.require(:user).permit(:name,:location,:email,:password,
                                  :password_confirmation)
   end
+  
+  #beforeフィルター
+    
+  def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+  
+  #正しいユーザーかどうか
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end  
 end
